477cc55ab265361b00ecaf90aa346be9ae24fb6e
Dimitrios Porfiropoulos <dimiporf@live.com>
2024-01-30 14:26:11 +0100

    Add project files.

---
+++ ImageMap/Controllers/ImageMapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImageMap.Controllers
+{
+    public class ImageMapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
